9484d4e84fb7e06c90d72d4403e9744f09d91670
Lacky227 <tishko.artyr@gmail.com>
2025-04-30 18:08:59 +0300

    feat(model-User): create model User

---
+++ authservice/src/main/java/com/veedev/authservice/model/User.java
@@ -0,0 +1,22 @@
+package com.veedev.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
