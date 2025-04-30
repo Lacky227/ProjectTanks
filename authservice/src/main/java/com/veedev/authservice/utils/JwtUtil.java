ab24e7a22fccf2ecd54bbee3fa1bb5e54c3a335c
Lacky227 <tishko.artyr@gmail.com>
2025-04-30 20:00:51 +0300

    feat(utils-JwtUtil): create JwtUtil for JWT token generation, username extraction, and validation

---
+++ authservice/src/main/java/com/veedev/authservice/utils/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.veedev.authservice.utils;
+
+import com.veedev.authservice.model.User;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret.key}")
+    private final String SECRET_KEY = "SecretKey";
+    private final long EXPIRATION_TIME = 86400000;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
